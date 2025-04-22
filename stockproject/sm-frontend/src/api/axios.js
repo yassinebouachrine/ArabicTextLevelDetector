0d6cab17d8c7a777e60f92887a5c42e7c79bbeab
Mohamed Khacha <145594540+0khacha@users.noreply.github.com>
2025-04-22 21:37:45 +0100

    Delete stockproject directory

--- stockproject/sm-frontend/src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
-    withCredentials: true, // This is necessary for Sanctum
-});
-export default api;
